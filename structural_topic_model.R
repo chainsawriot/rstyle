fb1d39cf428b5f2091ccd9dd4c13a6b78fb4a67f
chainsawriot <chainsawtiney@gmail.com>
2019-04-05 14:20:07 +0000

    STM and paper

--- structural_topic_model.R
+++ structural_topic_model.R
@@ -14,3 +14,22 @@ for (i in cran_views) {
     meta <- meta %>% mutate(!!(sym(tolower(i$name))) := pkg_name %in% i$packagelist$name)
 }
 
+require(stm)
+require(purrr)
+
+processed <- textProcessor(meta$desc, metadata = meta)
+out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
+
+map_chr(cran_views, "name") %>% tolower %>% paste(collapse = "+") %>% paste("~ ", .) %>% as.formula() -> prev_formula
+
+find_k <- manyTopics(out$documents,out$vocab, K=c(20, 40, 60, 80, 100), prevalence = prev_formula, max.em.its=100, run = 20, data=out$meta, seed=46709394, emtol = 1e-04)
+saveRDS(find_k, "stm_model.RDS")
+
+find_k <- readRDS('stm_model.RDS')
+as.formula(map_chr(cran_views, "name") %>% tolower %>% paste(collapse = "+") %>% paste("1:40~ ", .)) -> for40
+ 
+prep <- estimateEffect(stmobj = find_k$out[[2]], metadata = out$meta, formula = for40)
+
+stmes_tibble <- function(stmes) {
+    stmes[[1]] %>% as_tibble %>% mutate(var_name = row.names(stmes[[1]]))
+}
