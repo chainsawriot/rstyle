1719fcb8793eb1d88822592d378b6b3050f4eb5d
miachang <mia.chang@akelius.de>
2019-05-04 11:47:32 +0200

    depth 1 graph

--- build_package_graph.r
+++ build_package_graph.r
@@ -6,16 +6,11 @@ Imports_li <- desc$get("Imports")
 Suggests_li <-desc$get("Suggests")
 li<-strsplit(Imports_li, ",") 
 package_relate<-str_extract_all(li, "[a-z0-9]{2,}") 
-package_relate
-df <- data.frame("source"=character(), "dest"=character(), "weight"=numeric(),"depth"=numeric())
-
-for (i in package_relate) {
-  df$src = rbind(df$src, "ggplot2")
-  df$dest = rbind(df$dest, toString(i))
-  df$weight = rbind(df$weight, 1)
-  df$depth = rbind(df$depth, 1)
+package_relate_li<-(strsplit(package_relate[[1]], "\'"))
+df <- data.frame("id"=numeric(), "source"=character(), "dest"=character(), "weight"=numeric(),"depth"=numeric())
+count=0
+for (i in package_relate_li) { 
+  new_row <- data.frame("source"="ggplot2", "dest"=toString(i), "weight"=1,"depth"=1)
+  df<-rbind(df, new_row) 
 }
-df
-
-#g2 <- graph.data.frame(x, directed = TRUE)
-#plot(g2)
+df 
