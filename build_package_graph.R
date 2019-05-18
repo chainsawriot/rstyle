c74f747d8a92e3d0555e43be27024a9683f8c9c8
root <yen.chiayi@gmail.com>
2019-05-18 14:22:19 +0000

    refactor build_package_graph

---
+++ build_package_graph.R
@@ -0,0 +1,84 @@
+require(igraph)
+require(tidyverse)
+setwd('docker_data/rstyle/')#todo: make it automatically and more flexible
+
+
+
+get_dependency_snapshot <- function(type){
+    pkg_dependency <- readRDS('pkg_dependency.RDS')
+    if (type=="latest"){
+        snapshot <- pkg_dependency %>% group_by(pkg_name) %>% filter(pub_year==max(pub_year)) %>% ungroup()        
+    } else if (type=="cross-sectional") {
+        # TODO: aggregate across time
+    } else {
+        # TODO: raise error   
+    }
+    return(snapshot)
+}
+
+get_neibor_graph <- function(dependency, pkg_source){ # dependency as global variable?
+    df <- dependency %>% filter(pkg_name==pkg_source) %>% 
+        select(pkg_name, field, pkgs) %>% unnest() 
+    if (nrow(df) == 0){
+        # some packages have no fields like "Imports" or "Suggests"
+        # for example, stats (https://stat.ethz.ch/R-manual/R-devel/library/stats/html/stats-package.html)
+        return(NA)
+    }#TODO: better way to write it? the problem is at "pkgs"
+    df_rename <- df %>% 
+        transmute(source=pkg_name, dest=pkgs, weight=1, neibor_type=field) %>% 
+        filter(!is.na(dest))
+    return(df_rename)
+}
+
+get_neibor_graph_given_depth <- function(dependency, pkg_source, max_depth, type_dependency='latest'){
+    n_depth <- 1
+    pcks_source <- c(pkg_source)
+    pcks_has_detected <- c()
+    df <- NULL
+    
+    while (n_depth <= max_depth){
+        if (n_depth > 1){
+            print(df)
+            pcks_source <- df %>% filter(depth==n_depth-1) %>% pull(dest) %>% unique() %>% as.character()
+            pcks_has_detected <-  df %>% pull(source) %>% unique() %>% as.character()    
+        }
+        
+        for (pkg_source in pcks_source){
+            if (!pkg_source %in% pcks_has_detected){#todo: if using "break" then it breaks both for-loop and while-loop
+                tryCatch({
+                    sprintf('Depth: %s, Package: %s', n_depth, pkg_source) %>% print()
+                    df_depth <- get_neibor_graph(dependency, pkg_source) %>% mutate(depth=n_depth)
+                    df <- bind_rows(df, df_depth)
+                }, error = function(err) {
+                    sprintf('- %s can not found', pkg_source) %>% print()
+                })
+            }
+        }
+        # go to next depth
+        n_depth <- n_depth + 1
+    }
+    return(df)
+}
+
+###############
+#  configure
+max_depth <- 10
+type_dependency = 'latest'
+
+
+###############
+# trial: ggplot2
+dependency <- get_dependency_snapshot(type=type_dependency)#TODO: how can I put it outside the function safely for better performance?
+
+
+pkg_source <- 'ggplot2'
+df <- get_neibor_graph_given_depth(dependency, pkg_source, max_depth, type_dependency) 
+graph <- graph.data.frame(df,directed = TRUE)
+plot(graph)
+
+
+###############
+# trial: ggplot2
+#TODO: apply to all packages
+write_rds(df, "dependency_graph.RDS")
+
