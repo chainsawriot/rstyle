0faeb7b74474e677895e7ef1c0c719580b7d4a50
root <yen.chiayi@gmail.com>
2019-05-18 13:28:51 +0000

    switch dependency source from package-desc to RDS-made by extract_dependency.R

--- build_package_graph.r
+++ build_package_graph.r
@@ -1,79 +1,73 @@
-#import libraries
-library(desc) 
-library(stringr) 
-library(igraph)
-library(dplyr)
+require(igraph)
+require(tidyverse)
+setwd('docker_data/rstyle/')#todo: make it automatically and more flexible
 
 
-parse_desc_to_pck_names <-  function(raw_desc_pkg){
-  li <- strsplit(raw_desc_pkg, ",")[[1]]
-  package_relate <- str_extract_all(li, "[a-z0-9]{2,}") 
-  return(package_relate[[1]])
+
+get_dependency_snapshot <- function(type){
+    pkg_dependency <- readRDS('pkg_dependency.RDS')
+    if (type=="latest"){
+        snapshot <- pkg_dependency %>% group_by(pkg_name) %>% filter(pub_year==max(pub_year)) %>% ungroup()        
+    } else if (type=="cross-sectional") {
+        # TODO: aggregate across time
+    } else {
+        # TODO: raise error   
+    }
+    return(snapshot)
 }
 
-get_neibor_graph <- function(pkg_source, neibor_type='Imports'){
-  desc <- description$new(package = pkg_source)
-  raw_neibor_from_desc <- desc$get(neibor_type)
-  pkg_neibors <- parse_desc_to_pck_names(raw_neibor_from_desc)
-  df <- data.frame(
-    source = pkg_source,
-    dest = pkg_neibors,
-    weight = 1,
-    neibor_type=neibor_type
-  )
-  return(df)
+get_neibor_graph <- function(pkg_source, type_dependency='latest'){
+    dependency <- get_dependency_snapshot(type=type_dependency)#todo: how can I put it outside the function safely for better performance?
+    df <- dependency %>% filter(pkg_name==pkg_source) %>% 
+        select(pkg_name, field, pkgs) %>% unnest() %>% 
+        transmute(source=pkg_name, dest=pkgs, weight=1, neibor_type=field, depth=1)
+    return(df)
 }
 
-get_neibor_graph_given_depth <- function(pck_name, max_depth){
-  df <- data.frame("source"=character(), 
-                   "dest"=character(), 
-                   "weight"=numeric(),
-                   "neibor_type"=character(), 
-                   "depth"=numeric())
-  
-  n_depth <- 1
-  while (n_depth <= max_depth){
-  
-      # collect source packags 
-      if (n_depth == 1){
-        pcks_source <-  c(pck_name) 
-      } else {
+
+
+get_neibor_graph_given_depth <- function(pkg_source, max_depth, type_dependency='latest'){
+    # neibor_type
+    df <- get_neibor_graph(pkg_source, type_dependency)
+    n_depth <- max(df$depth) + 1
+    while (n_depth <= max_depth){
+        # collect source packag
         pcks_source <-  df %>% filter(depth==n_depth-1) %>% pull(dest) %>% unique() %>% as.character()
-      }
-      print(pcks_source)
-      
-      # get neibor of each source package
-      pcks_has_detected <-  df  %>% pull(source) %>% unique() %>% as.character()
-      for (pck_source in pcks_source){
-        if (!pck_source %in% pcks_has_detected){
-          tryCatch({
-            sprintf('Depth: %s, Package: %s', n_depth, pck_source) %>% print()
-            
-            df_imports <- get_neibor_graph(pck_source, neibor_type='Imports')
-            df_suggests <- get_neibor_graph(pck_source, neibor_type='Suggests')
-            df_depth <- rbind(df_imports, df_suggests) %>% filter(!is.na(dest))
-            df_depth['depth'] <- n_depth
-            df <- rbind(df, df_depth)  
-            
-            }, error = function(err) {
-              sprintf('- %s can not found', pck_source) %>% print()
-            })
+        
+        # get neibor of each source package
+        pcks_has_detected <-  df %>% pull(source) %>% unique() %>% as.character()
+        for (pkg_source in pcks_source){
+            if (!pkg_source %in% pcks_has_detected){
+                tryCatch({
+                    sprintf('Depth: %s, Package: %s', n_depth, pkg_source) %>% print()
+                    
+                    # df_imports <- get_neibor_graph(pkg_source, neibor_type='Imports')
+                    # df_suggests <- get_neibor_graph(pkg_source, neibor_type='Suggests')
+                    # df_depth <- rbind(df_imports, df_suggests) %>% filter(!is.na(dest))
+                    # df_depth['depth'] <- n_depth <- ≠
+                    df_depth <- get_neibor_graph(pkg_source, type_dependency) 
+                    df <- bind_rows(df, df_depth)
+                }, error = function(err) {
+                    sprintf('- %s can not found', pkg_source) %>% print()
+                })
+            }
         }
-      }
-      # go to next depth
-      n_depth <- n_depth + 1
-  }
-  return(df)
+        # go to next depth
+        n_depth <- n_depth + 1
+    }
+    return(df)
 }
 
 
-df <- get_neibor_graph_given_depth("ggplot2", max_depth = 10)
-import_plot<-graph.data.frame(df,directed = TRUE)
-plot(import_plot)
+pkg_source <- 'ggplot2'
+df <- get_neibor_graph_given_depth(pkg_source, max_depth = 10, type_dependency = 'latest') 
+df <- df %>% filter(!is.na(dest), neibor_type=="imports")
+graph <- graph.data.frame(df,directed = TRUE)
+plot(graph)
 
 
 
 
 # community walktrap
-communities <- cluster_walktrap(import_plot)
+communities <- cluster_walktrap(graph)
 communities
