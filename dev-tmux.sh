b27dcc7498c691efb4d71ef97b2218df71979325
cyyen <yen.chiayi@gmail.com>
2019-05-31 21:33:09 +0800

    make dev-tmux.sh more readable

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -3,4 +3,7 @@ tmux new-session \; \
 split-window -v -p 60 \; \
   send-keys 'git log --graph --all --oneline --decorate' ENTER \; \
 split-window -h \; \
-  send-keys 'docker container stop rstyle; docker container rm rstyle; docker run --name rstyle -d -v $(pwd):/home/$USER/rstyle -e USER=$USER -e PASSWORD=0000 -e USERID=$UID -p 8787:8787 rstudio/rstyle; docker ps | grep rstyle' ENTER \; \
+  send-keys '   docker container stop rstyle; 
+                docker container rm rstyle; 
+                docker run --name rstyle -d -v $(pwd):/home/$USER/rstyle -e USER=$USER -e PASSWORD=0000 -e USERID=$UID -p 8787:8787 rstudio/rstyle; 
+                docker ps | grep rstyle' ENTER \; \
