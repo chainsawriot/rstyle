38f9af19082b5526268128d6264eaeaa23306c8a
cyyen <yen.chiayi@gmail.com>
2019-06-27 06:26:09 +0800

    plot naming among community / wide-to-long for ggplot2

--- community_based_features_visualization.R
+++ community_based_features_visualization.R
@@ -5,15 +5,17 @@ df_ratio_total <- readRDS('community_df_ratio_total.RDS')
 df_naming_total <- readRDS('community_df_naming_total.RDS')
 df_naming_ratio_total <- readRDS('community_df_naming_ratio_total.RDS')
 
-key <- 'alllower'
 
-
+wide_to_long <- function(df, id_var, key_var, val_var, sort_by){
+    df %>% 
+        t() %>% as.data.frame() %>% 
+        rownames_to_column(id_var) %>% 
+        arrange(UQ(sort_by)) %>% 
+        gather(!!key_var, !!val_var, -(!!id_var)) 
+}
 
 df_naming_ratio_long <- df_naming_ratio_total %>% 
-    t() %>% as.data.frame() %>% 
-    rownames_to_column('community') %>% 
-    arrange(snake) %>% 
-    gather('naming', 'ratio', -community) %>% 
+    wide_to_long(id_var = 'community', key_var = 'naming', val_var = 'ratio', sort_by = quo(snake)) %>% 
     mutate(percentage = ratio*100, 
            community=factor(community, levels = unique(community)),
            naming=factor(naming, levels = c('dotted', 'allupper', 'alllower', 'other', 'upcamel', 'lowcamel', 'snake'))) 
@@ -27,3 +29,8 @@ g_naming <- ggplot() +
 png("naming_among_community.png", width = 9*96, height = 6*96)
 g_naming
 dev.off()
+
+
+# cake plot
+df_ratio_long <- df_ratio_total %>% 
+    wide_to_long(id_var = 'community', key_var = 'feature', val_var = 'ratio', sort_by = quo(fx_assign)) 
