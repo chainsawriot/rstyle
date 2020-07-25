154ab90bbbb2428748ad8f2a391445fb329d3abb
chainsawriot <chainsawtiney@gmail.com>
2020-07-25 15:20:07 +0200

    Organizing fun analysis' files into yen's convention

--- config.R
+++ config.R
@@ -1,3 +1,5 @@
+## We'll analyze all packages published in <= INCLUDE_YR
+INCLUDE_YR <- 2019
 
 PATH_CODE_DB <- 'code.db'
 PATH_PKGS_FUNCTIONS_W_SYNTAX_FEATURE <- "pkgs_functions_with_syntax_feature.RDS"
@@ -11,3 +13,7 @@ PATH_COMM <- "data/comm_walktrap.RDS"
 PATH_COMM_EVCENT <- "data/comm_evcent.RDS"
 PATH_COMM_SIZE <- "data/comm_size.RDS"
 PATH_COMM_LANG_FEATURES <- "data/comm_lang_feature.RDS"
+
+## fun0* -related parameters
+FX_DATA_PREFIX <- "data/fx_data_yr"
+FX_DATA_SUFFIX <- ".RDS"
