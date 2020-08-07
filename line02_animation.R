30fe18cca1d097ee96b54b72056d455eae0430c8
chainsawriot <chainsawtiney@gmail.com>
2020-08-07 14:15:41 +0200

    line vis

--- line02_animation.R
+++ line02_animation.R
@@ -1,11 +1,15 @@
+require(tidyverse)
+require(gganimate)
+require(modules)
 
-comment_dist <- readRDS('comment_dist.RDS')
+cfg <- modules::use("config.R")
 
+comment_dist <- readRDS(cfg$PATH_COMMENT_DIST)
 
 
 comment_dist %>% mutate(comment = ifelse(comment == 1, "Yes", "No")) %>%  group_by(pub_year, comment) %>% mutate(totalline = sum(n), prob = (n / totalline)) %>% 
-    select(pub_year, n_chars, prob, comment) %>% filter(pub_year %in% c(2010, 2018)) %>%
-    filter(n_chars > 40 & n_chars < 100 & pub_year < 2019) %>% 
+    select(pub_year, n_chars, prob, comment) %>% filter(pub_year %in% c(2010, 2019)) %>%
+    filter(n_chars > 40 & n_chars < 100 & pub_year <= 2019) %>% 
     mutate(prob = prob * 100) %>%
     ggplot(aes(x = n_chars, y = prob, color = comment)) + 
     geom_line(stat = 'identity') +
@@ -14,21 +18,20 @@ comment_dist %>% mutate(comment = ifelse(comment == 1, "Yes", "No")) %>%  group_
     theme(plot.title = element_text(size = 24, face = "bold"), plot.subtitle =  element_text(size = 10), axis.text = element_text(size = 15), axis.title=element_text(size=14,face="bold")) + 
     theme(rect = element_rect(fill = "transparent")) +
     theme(legend.position = "none") + theme(strip.text.x = element_text(size = 15, face = "bold")) -> line_compare
-ggsave('line_length_2010_2018.png', line_compare, width = 6, height = 5, units = 'in', bg = "transparent")
+ggsave('visualization_line/line_length_2010_2019.png', line_compare, width = 6, height = 5, units = 'in', bg = "transparent")
 
-require(gganimate)
-comment_dist %>% mutate(comment = comment == 1) %>%  group_by(pub_year, comment) %>% mutate(totalline = sum(n), prob = (n / totalline) * 100) %>% select(pub_year, n_chars, prob, comment) %>% filter(n_chars < 150 & pub_year < 2019) %>% 
+comment_dist %>% mutate(comment = comment == 1) %>%  group_by(pub_year, comment) %>% mutate(totalline = sum(n), prob = (n / totalline) * 100) %>% select(pub_year, n_chars, prob, comment) %>% filter(n_chars < 150 & pub_year <= cfg$INCLUDE_YR) %>% 
   ggplot(aes(x = n_chars, y = prob, color = comment)) + 
   geom_line(stat = 'identity') +
   geom_vline(xintercept = 80, alpha = 0.1) + facet_wrap(~ pub_year, nrow = 3)
 
 require(glue)
 comment_dist %>% mutate(comment = comment == 1) %>%  group_by(pub_year, comment) %>% mutate(totalline = sum(n), prob = (n / totalline) * 100) %>% 
-  select(pub_year, n_chars, prob, comment) %>% filter(n_chars < 150 & pub_year < 2019) %>%
+  select(pub_year, n_chars, prob, comment) %>% filter(n_chars < 150 & pub_year <= cfg$INCLUDE_YR) %>%
   ungroup %>% ggplot(aes(x = n_chars, y = prob, color = comment)) + 
   geom_line(stat = 'identity') +
   geom_vline(xintercept = 80, alpha = 0.1) +
-  labs(title = 'Year: {frame_time}', x = 'Number of Characters', y = 'Proportion') +
+  labs(title = 'Year: {frame_time}', x = 'Number of Characters', y = 'Share of all lines (%)') +
   transition_time(pub_year)
 
 comment_dist %>% mutate(comment = comment == 1) %>% filter(n_chars > 1) %>%  group_by(pub_year, comment) %>% mutate(totalline = sum(n), prob = (n / totalline) * 100) %>% 
